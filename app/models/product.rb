0ccf1619a9c59887de5d13b06f3762bdf9636be6
Junior Abreu <juniorabreu201@gmail.com>
2022-08-06 17:24:15 -0400

    add query of the three most purchased products by category

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,20 @@ class Product < ApplicationRecord
   belongs_to :category
 
   validates :name, :price, :category_id, presence: true
+
+  def self.most_selled_products
+    ActiveRecord::Base.connection.exec_query("
+      select * from categories c
+      join lateral (
+        select p.category_id , sum(b.quantity) vendido, p.id product_id
+      from buies b
+      inner join products p on p.id = b.product_id
+      where p.category_id = c.id
+      group by p.category_id , p.id order by vendido desc limit 3
+
+      ) p on true
+      order by c.id;
+
+    ").to_a
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+  categories = []
+  3.times.each do |algo|
+    categories.push Category.create({ name: Faker::Name.name })
+  end
+
+  products = []
+  10.times.each do |algo|
+    products.push Product.create({name:Faker::Device.manufacturer, price: Faker::Number.decimal(l_digits: 2), category_id: categories.sample.id })
+  end
+
+  Buy.upsert_all(
+    50000.times.map do |algo|
+      product = products.sample
+      quantity = Faker::Number.between(from: 1, to: 10)
+      price = product.price
+      total = price * quantity
+      {
+        product_id: product.id,
+        price: price,
+        total: total,
+        quantity: quantity
+      }
+    end
+  )
